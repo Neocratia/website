81d2011ab1fc058cf78b41e586da1a3ec56e6a9c
Andres Gonzalez <andresgz@gmail.com>
2017-10-14 21:03:08 -0400

    Adjustments after usability test

--- pages/index.vue
+++ pages/index.vue
@@ -354,12 +354,12 @@
                 <v-icon x-large primary>mdi-facebook</v-icon>
               </v-btn>
             </a>
-            <a href="https://api.whatsapp.com/send?text=Check https%3A//www.neocratia.org/" data-action="share/whatsapp/share" class="button" target="_blank">
+            <a href="https://api.whatsapp.com/send?text=Let’s upgrade the way our laws are created https%3A//www.neocratia.org/" data-action="share/whatsapp/share" class="button" target="_blank">
               <v-btn light class="square">
                 <v-icon x-large primary>mdi-whatsapp</v-icon>
               </v-btn>
             </a>
-            <a href="https://twitter.com/home?status=Check https%3A//www.neocratia.org/" class="button" target="_blank">
+            <a href="https://twitter.com/home?status=Let’s upgrade the way our laws are created https%3A//www.neocratia.org/" class="button" target="_blank">
               <v-btn light class="square">
                 <v-icon x-large primary>mdi-twitter</v-icon>
               </v-btn>
