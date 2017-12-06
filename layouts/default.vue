8775e953bcbe11dcb94a2410ecb2715587161e79
Andres Gonzalez <andres@swapps.co>
2017-12-05 20:08:53 -0500

    Implement Team page with navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,30 @@
 <template>
 <v-app toolbar footer>
+  <v-navigation-drawer persistent clipped enable-resize-watcher :floating="false" v-model="drawer">
+    <v-list>
+      <v-list-tile router v-for="(item, i) in items" :key="i" :to="item.to">
+        <v-list-tile-action>
+          <v-icon v-html="item.icon"></v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="item.title"></v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
   <v-toolbar fixed>
+    <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
     <v-toolbar-title>
       <img src="/logo.png" alt="neocratia logo">
     </v-toolbar-title>
     <v-spacer></v-spacer>
   </v-toolbar>
   <main>
-    <v-container fluid main-container>
+    <v-container fluid>
       <nuxt />
     </v-container>
   </main>
+
 </v-app>
 </template>
 
@@ -18,10 +32,12 @@
 export default {
   data() {
     return {
+      drawer: true,
+      fixed: false,
       items: [{icon: 'home', title: 'Home', to: '/'},
-              {icon: 'people', title: 'About us', to: '/inspire'},
-              {icon: 'mail', title: 'Contact', to: '/contact'}
+              {icon: 'people', title: 'Team', to: '/team'},
              ],
+      title: 'Neocratia'
     }
   }
 }
