87cf468e46b12b0aed50e9cf0bbd9542ace66a19
Andres Gonzalez <andres@swapps.co>
2017-12-08 15:18:23 -0500

    Add team page based on design

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,30 +1,25 @@
 <template>
 <v-app toolbar footer>
-  <v-navigation-drawer persistent clipped enable-resize-watcher :floating="false" v-model="drawer">
-    <v-list>
-      <v-list-tile router v-for="(item, i) in items" :key="i" :to="item.to">
-        <v-list-tile-action>
-          <v-icon v-html="item.icon"></v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title v-text="item.title"></v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
-  </v-navigation-drawer>
   <v-toolbar fixed>
-    <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
     <v-toolbar-title>
+      <nuxt-link to="/">
       <img src="/logo.png" alt="neocratia logo">
+      </nuxt-link>
+      </a>
     </v-toolbar-title>
     <v-spacer></v-spacer>
+    <div class="navigation-bar">
+      <nuxt-link to="/team">
+      OUR TEAM
+      </nuxt-link>
+    </a>
+    </div>
   </v-toolbar>
   <main>
-    <v-container fluid>
+    <v-container fluid main-container>
       <nuxt />
     </v-container>
   </main>
-
 </v-app>
 </template>
 
@@ -32,12 +27,10 @@
 export default {
   data() {
     return {
-      drawer: true,
-      fixed: false,
       items: [{icon: 'home', title: 'Home', to: '/'},
-              {icon: 'people', title: 'Team', to: '/team'},
+              {icon: 'people', title: 'About us', to: '/inspire'},
+              {icon: 'mail', title: 'Contact', to: '/contact'}
              ],
-      title: 'Neocratia'
     }
   }
 }
--- pages/team.vue
+++ pages/team.vue
@@ -1,98 +1,94 @@
 <template>
-<v-layout column align-center>
+<v-layout column align-center class="page-team">
   <v-flex xs12 sm10 md8 lg6 elevation-1 content-area>
     <v-card class="py-3 px-3">
       <v-layout fill-height justify-center>
         <v-card-title primary-title class="text-xs-center headline">
-          Our Team
+          At Neocratia, we're all volunteers.
         </v-card-title>
       </v-layout>
-      <v-layout row wrap>
+      <v-layout row wrap mt-4>
           <v-flex xs12>
-            <v-card class=" darken-2 ">
+            <v-card class=" darken-2 pa-2">
               <v-container fluid grid-list-md>
                 <v-layout row>
-                  <v-flex xs12>
+                  <v-flex xs6 sm7 md8>
                     <div>
                       <div class="headline">Alexander Torrenegra</div>
-                      <div>Alexander Torrenegra is a Colombian-American entrepreneur. He founded multiple companies including Zapata Torrenegra Labs, Voice123, VoiceBunny and Torre. He was featured in MIT’s TR35 Colombia’s list of Top Innovators Under 35 in 2012. In 2013, he was part of a delegation of immigrant-entrepreneurs that met with President Barack Obama to push for immigration reform. He also works to improve government support for innovation in Colombia and is the founder of Neocratia.</div>
+                      <div>Alexander Torrenegra is a Colombian-American activist and entrepreneur. He founded multiple companies including Torre, Bunny Inc., and Voice123. He was featured in MIT’s Technology Review as Colombia’s Top Young Innovator of 2012. In 2015, he joined the World Economic Forum’ Young Global Leaders community. In the US, Alexander is an activist that pushes for immigration reform. In Colombia, he works to improve government support for innovation. Alexander is the founder of Neocratia.</div>
                     </div>
                   </v-flex>
-                  <v-flex xs12>
-                    <v-card-media
-                      src="/img/team/alex.jpg"
-                      height="320px"
-                      contain
-                    ></v-card-media>
+                  <v-flex xs6 sm5 md4 text-xs-right>
+                    <img src="~/assets/team/alex-bw.png" height="120">
                   </v-flex>
                 </v-layout>
+                  <div class="text-xs-right">
+                    <a href="https://bio.torre.co/torrenegra" target="_blank">LEARN MORE</a>
+                  </div>
               </v-container>
             </v-card>
           </v-flex>
           <v-flex xs12>
-            <v-card class=" darken-2 mt-4">
+            <v-card class=" darken-2 mt-4 pa-2">
               <v-container fluid grid-list-md>
                 <v-layout row>
-                  <v-flex xs12>
-                    <v-card-media
-                      src="/img/team/andres.jpg"
-                      height="320px"
-                      contain
-                    ></v-card-media>
-                  </v-flex>
-                  <v-flex xs12>
+                  <v-flex xs6 sm7 md8>
                     <div>
                       <div class="headline">Andrés González</div>
-                      <div>Andres is a software developer, entrepreneur and tech enthusiast. He has been involved in the implementation of several software products during more than 10 years. He joined Neocratia as a volunteer in 2015 after a hackathon in Bogotá. Since then he has been involved in the product development and now he is the maintainer of the Neocratium platform.</div>
+                      <div>Andres is a software developer, entrepreneur and tech enthusiast. Originary from Colombia, he has been in the software industry for more than 10 years leading projects for companies around the world. He is the founder and technical leader of Swapps a software development company based in  Colombia. He is the maintainer of the Neocratium platform.</div>
                     </div>
+                  </v-flex>
+                  <v-flex xs6 sm5 md4 text-xs-right>
+                    <img src="~/assets/team/andres-bw.jpg" height="120">
                   </v-flex>
 
                 </v-layout>
+                <div class="text-xs-right">
+                  <a href="https://www.linkedin.com/in/andresgz/" target="_blank">LEARN MORE</a>
+                </div>
               </v-container>
             </v-card>
           </v-flex>
 
           <v-flex xs12>
-            <v-card class=" darken-2 mt-4">
+            <v-card class=" darken-2 mt-4 pa-2">
               <v-container fluid grid-list-md>
                 <v-layout row>
-                  <v-flex xs12>
+                  <v-flex xs6 sm7 md8>
                     <div>
                       <div class="headline">Helena Schwertheim</div>
                       <div>Helena is passionate about innovating for a more accountable democracy. Originally from New Zealand, she has a background in political studies, sociology and international relations, having lived and studied in Australia, Colombia, Spain and France, and is now based in Sweden. During the day Helena is a researcher, analysing democratic trends. By night she works with Neocrats to upgrade how laws are made.</div>
                     </div>
                   </v-flex>
-                  <v-flex xs12>
-                    <v-card-media
-                      src="/img/team/helena.jpg"
-                      height="320px"
-                      contain
-                    ></v-card-media>
+                  <v-flex xs6 sm5 md4 text-xs-right>
+                    <img src="~/assets/team/helena-bw.jpg" height="120">
                   </v-flex>
                 </v-layout>
+                <div class="text-xs-right">
+                  <a href="https://www.linkedin.com/in/helenaschwertheim/" target="_blank">LEARN MORE</a>
+                </div>
               </v-container>
             </v-card>
           </v-flex>
 
           <v-flex xs12>
-            <v-card class=" darken-2 mt-4">
+            <v-card class=" darken-2 mt-4 pa-2">
               <v-container fluid grid-list-md>
                 <v-layout row>
-                  <v-flex xs12>
-                    <v-card-media
-                      src="/img/team/rolf.jpg"
-                      height="320px"
-                      contain
-                    ></v-card-media>
-                  </v-flex>
-                  <v-flex xs12>
+                  <v-flex xs6 sm7 md8>
                     <div>
                       <div class="headline">Rolf Veldman</div>
                         <p>Rolf is a project manager and change manager with a passion for creative writing and 'Learning organizations'. </p>
                         <p>Curiosity is the driving force behind his ambition. This attitude leads Rolf to be creative, assertive and open minded. Fueled by his academic education in political history Rolf has always had a more than average interest in politics. He joined Neocratia in November 2017 and is responsible for Recruitment, Fundraising and Political Advice.</p> 
                     </div>
                   </v-flex>
+                  <v-flex xs6 sm5 md4 text-xs-right>
+                    <img src="~/assets/team/rolf-bw.jpg" height="120">
+                  </v-flex>
                 </v-layout>
+                <div class="text-xs-right">
+                  <a href="https://www.linkedin.com/in/rolfveldman/" target="_blank">LEARN MORE</a>
+                </div>
               </v-container>
             </v-card>
           </v-flex>
@@ -176,6 +172,11 @@ var axios = require('axios');
 const API_URL = process.env.api_url;
 
 export default {
+  head () {
+    return {
+      titleTemplate: 'Our Team | Neocratia'
+    }
+  },
   data() {
     return {
       valid: false,
