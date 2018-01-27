1b9d7c0d33b9417442fa2b2fa205ca1e779480aa
Andres Gonzalez <andres@swapps.co>
2018-01-26 23:30:31 -0500

    Adjustments on content

--- components/home-headline.vue
+++ components/home-headline.vue
@@ -2,7 +2,8 @@
 <v-card class="primary white--text py-5">
   <p class="subheading text-xs-center px-3">
     {{ $t('home.subheading.line1') }}.<br/><br/>
-    {{ $t('home.subheading.line2') }}.
+    {{ $t('home.subheading.line2') }}.<br/><br/>
+    {{ $t('home.subheading.line3') }}.
   </p>
 </v-card>
 </template>
